6aaf67d491468034bb10cf1ab4f0a16b3d2749bc
danielto2910 <dto1@ggc.edu>
2025-02-15 18:15:24 -0500

    import Logo

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView,Image } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images} from '../constants'
 
 const index = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Tokiro!</Text>
-      <Link href="/home" className='text-blue-500'>Go To Home</Link>
-    </View>
+    <SafeAreaView className= "bg-primary h-full">
+      <ScrollView contentContainerStyle = {{ height: "100%"}}>
+        <View className = "w-full px-4 justify-center items-center">
+          <Image
+            source={images.logo}
+            className="w-[330px] h-[200px]"
+            resizeMode='contain'
+          />
+
+          {/* <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode='contain'
+          /> */}
+        </View>
+
+        <View className= "mt-5 px-5">
+          <Text className= "text-3xl text-white font-bold text-center">Level up your productivity with{' '} 
+          <Text className= "text-secondary-200">Tokiro</Text>
+          </Text>
+          
+        </View>
+
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
 export default index
-
-const styles = StyleSheet.create({})
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#161622",
+        primary: "#1f1f1f",
         secondary: {
           DEFAULT: "#FF9C01",
           100: "#FF9001",
