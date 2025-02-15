5eda895c4dbace0924ee263343ce5f147bb7f198
danielto2910 <dto1@ggc.edu>
2025-02-14 22:36:42 -0500

    imported Fonts and Setup different Screens

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Link } from 'expo-router'
 
 const index = () => {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl text-blue-500">index</Text>
+      <Text className="text-3xl text-blue-500 font-pblack">index</Text>
+      {/* <Link href=""></Link> */}
     </View>
   )
 }
