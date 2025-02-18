5b2703c4b54d03f6be5f5603aa3c133b79ee6728
danielto2910 <dto1@ggc.edu>
2025-02-18 18:45:16 -0500

    added create and clean out some things

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,7 +1,10 @@
-import { StyleSheet, Text, View, Image, useWindowDimensions} from 'react-native'
+import { StyleSheet, Text, View, Image, useWindowDimensions, TouchableOpacity} from 'react-native'
 import { Tabs, Redirect } from 'expo-router'
 import { icons } from '../../constants'
-import React from 'react'
+import React, { useRef , useState} from "react";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
 
 const TabIcon = ({icon, color, name, focused})=> {
     const { height } = useWindowDimensions();
@@ -20,91 +23,152 @@ const TabIcon = ({icon, color, name, focused})=> {
 }
 
 const TabLayout = () => {
+    const [isBottomSheetVisible, setBottomSheetVisible] = useState(false);  // State to control BottomSheet visibility
+    const bottomSheetRef = useRef(null);
+
+    const handleOpen = () => {
+        setBottomSheetVisible(true);  // Show the BottomSheet
+        bottomSheetRef.current?.expand();  // Optionally expand the BottomSheet
+    };
+
+    const handleClose = () => {
+        setBottomSheetVisible(false);  // Hide the BottomSheet
+        bottomSheetRef.current?.close();  // Optionally close the BottomSheet
+    };
+
   return (
     <>
-        <Tabs screenOptions={{
-            tabBarShowLabel: false,
-            tabBarActiveTintColor: '#b9dbb3',
-            tabBarInactiveTintColor: '#a0a7a4',
-            tabBarStyle:{
-                backgroundColor:"#151515", 
-                borderTopWidth: 1,
-                borderTopColor: "#232533",
-                height: 85,
-            }
-        }}>
-            <Tabs.Screen
-                name='home'
-                options={{
-                    title: 'Home',
-                    headerShown: false,
-                    tabBarIcon: ({ color, focused }) => (
-                        <TabIcon
-                            icon={icons.home}
-                            color={color}
-                            name="Home"
-                            focused={focused}
-                        />
-                    )
+     <GestureHandlerRootView style={styles.container}>
+            <Tabs screenOptions={{
+                tabBarShowLabel: false,
+                tabBarActiveTintColor: '#b9dbb3',
+                tabBarInactiveTintColor: '#a0a7a4',
+                tabBarStyle:{
+                    backgroundColor:"#151515", 
+                    borderTopWidth: 1,
+                    borderTopColor: "#232533",
+                    height: 85,
+                }
+            }}>
+                <Tabs.Screen
+                    name='home'
+                    options={{
+                        title: 'Home',
+                        headerShown: false,
+                        tabBarIcon: ({ color, focused }) => (
+                            <TabIcon
+                                icon={icons.home}
+                                color={color}
+                                name="Home"
+                                focused={focused}
+                            />
+                        )
 
-                }}
-            />
+                    }}
+                />
 
-            <Tabs.Screen
-                name='task'
-                options={{
-                    title: 'Task',
-                    headerShown: false,
-                    tabBarIcon: ({ color, focused }) => (
-                        <TabIcon
-                            icon={icons.bookmark}
-                            color={color}
-                            name="Task"
-                            focused={focused}
-                        />
-                    )
+                <Tabs.Screen
+                    name='task'
+                    options={{
+                        title: 'Task',
+                        headerShown: false,
+                        tabBarIcon: ({ color, focused }) => (
+                            <TabIcon
+                                icon={icons.bookmark}
+                                color={color}
+                                name="Task"
+                                focused={focused}
+                            />
+                            
+                        ),
+                        
 
-                }}
-            />
+                    }}
+                />
+                
+                <Tabs.Screen
+                    name='create'
+                    options={{
+                        title: 'Create',
+                        headerShown: false,
+                        tabBarIcon: ({ color, focused }) => (
+                            <TabIcon
+                                icon={icons.plus}
+                                color={color}
+                                name="Create"
+                                focused={focused}
 
-            <Tabs.Screen
-                name='shop'
-                options={{
-                    title: 'Shop',
-                    headerShown: false,
-                    tabBarIcon: ({ color, focused }) => (
-                        <TabIcon
-                            icon={icons.shop}
-                            color={color}
-                            name="Shop"
-                            focused={focused}
-                        />
-                    )
+                            />
+                        ),
+                        tabBarButton: (props) => <TouchableOpacity {...props} onPress={handleOpen}/>
+                    }}
+                />
 
-                }}
-            />
+                <Tabs.Screen
+                    name='shop'
+                    options={{
+                        title: 'Shop',
+                        headerShown: false,
+                        tabBarIcon: ({ color, focused }) => (
+                            <TabIcon
+                                icon={icons.shop}
+                                color={color}
+                                name="Shop"
+                                focused={focused}
+                            />
+                        )
 
-            <Tabs.Screen
-                name='profile'
-                options={{
-                    title: 'Profile',
-                    headerShown: false,
-                    tabBarIcon: ({ color, focused }) => (
-                        <TabIcon
-                            icon={icons.profile}
-                            color={color}
-                            name="Profile"
-                            focused={focused}
-                        />
-                    )
+                    }}
+                />
 
-                }}
-            />
-        </Tabs>
+                <Tabs.Screen
+                    name='profile'
+                    options={{
+                        title: 'Profile',
+                        headerShown: false,
+                        tabBarIcon: ({ color, focused }) => (
+                            <TabIcon
+                                icon={icons.profile}
+                                color={color}
+                                name="Profile"
+                                focused={focused}
+                            />
+                        )
+
+                    }}
+                />
+            </Tabs>
+
+            {/* 🔹 Bottom Sheet Component */}
+            
+            {isBottomSheetVisible && (
+                    <BottomSheet 
+                        ref={bottomSheetRef} 
+                        enablePanDownToClose={true}
+                        snapPoints={[650]}
+                    >
+                        <BottomSheetView style={styles.contentContainer}>
+                            <Text style={{ fontSize: 18, fontWeight: 'bold', textAlign: 'center' }}>
+                                Create Something 🎉
+                            </Text>
+                        </BottomSheetView>
+                    </BottomSheet>
+                )}
+        </GestureHandlerRootView>
     </>
   )
 }
 
 export default TabLayout
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#eeefc2",
+    },
+    contentContainer: {
+      flex: 1,
+      padding: 36,
+      alignItems: 'center',
+    },
+  });
